3ee5756d1a67ca05796cf776cbcbb0092f4dfa69
SanderJohansen <55443844+SanderJohansen@users.noreply.github.com>
2021-11-11 17:17:23 +0100

    prototype to create course

--- web/src/pages/CourseSingle.js
+++ web/src/pages/CourseSingle.js
@@ -1,6 +1,8 @@
 import {useParams} from "react-router-dom";
 import CourseStore from "../store/CourseStore";
 import {useEffect, useState} from "react";
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const courseStore = new CourseStore();
 
@@ -27,7 +29,24 @@ export default function CourseSingle() {
     return (
         <div>
             <h1>
-                {courseData.id}
+                <Grid spacing={1}
+                > <Typography variant="h3" component="div"> {courseData.name} </Typography>  </Grid>
+                <Grid container
+                      spacing={2}
+                      padding={2}
+                      direction="column"
+                      justifyContent="Flex-start"
+                      alignItems="Flex-start"
+                >
+
+                    <Grid item>  <Typography variant="h5" > Course number: {courseData.id} </Typography> </Grid>
+                    <Grid item> <Typography variant="body2" > {courseData.weekday} at {courseData.time}</Typography> </Grid>
+
+                    <Grid item> <Typography variant="body2" >The maximum number of participants is {courseData.maxcap}</Typography></Grid>
+                    <Grid item> <Typography variant="body2" >Ects: 5</Typography></Grid>
+                    <Grid item> <Typography variant="body2" >Teacher: H.C Andersen</Typography></Grid>
+
+                </Grid>
             </h1>
         </div>
     );
