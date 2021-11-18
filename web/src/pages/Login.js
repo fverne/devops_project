5dc0d37492c53dce9c020ff49c587f1a034e8553
SanderJohansen <55443844+SanderJohansen@users.noreply.github.com>
2021-11-18 09:54:41 +0100

    login page recreated

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -3,10 +3,11 @@ import Box from "@mui/material/Box";
 import {FormControl, Input, InputLabel} from "@mui/material";
 import {tokenStore} from "../store/TokenStore";
 import Button from "@mui/material/Button";
+import {CardMedia} from "@material-ui/core";
 
 export default function Login() {
     return(
-        <Box
+        <Grid
             component="form"
             sx={{
                 '& > :not(style)': { m: 1 },
@@ -14,17 +15,36 @@ export default function Login() {
             noValidate
             autoComplete="off"
         >
-            <FormControl variant="standard">
-                <InputLabel htmlFor="username" value={tokenStore.logindata.username}>Username</InputLabel>
-                <Input id="username" onChange={(e) => tokenStore.logindata.username=e.target.value}/>
-            </FormControl>
-            <FormControl variant="standard">
-                <InputLabel htmlFor="password" value={tokenStore.logindata.password}>Password</InputLabel>
-                <Input id="password" onChange={(e) => tokenStore.logindata.password=e.target.value}/>
-            </FormControl>
-            <FormControl variant="standard">
-                <Button onClick={()=>tokenStore.doLogin()}>Login</Button>
-            </FormControl>
-        </Box>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                    <h2>DTU INSIDE</h2>
+                </Grid>
+
+                <FormControl variant="standard" fullWidth>
+                    <InputLabel htmlFor="username" value={tokenStore.logindata.username}  fullWidth>Username</InputLabel>
+                    <Input id="username" onChange={(e) => tokenStore.logindata.username=e.target.value} />
+                </FormControl>
+                <FormControl variant="standard" fullWidth>
+                    <InputLabel htmlFor="password"  value={tokenStore.logindata.password} fullWidth>Password</InputLabel>
+                    <Input id="password"  onChange={(e) => tokenStore.logindata.password=e.target.value } />
+                </FormControl>
+
+
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth onClick={()=>tokenStore.doLogin()}>Sign in</Button>
+                <Button variant="contained" color="secondary" style={btnstyle} fullWidth  > Login with inside</Button>
+                <grid>
+
+                    <CardMedia
+
+                        src="C:\Users\sande\Documents\GitHub\login_page\src\dtu_icon.jpg"
+                        title="dtu icon"
+                        style={{width: "100%", height: "100%", objectFit: "auto"}}
+
+                    />
+                </grid>
+
+            </Paper>
+        </Grid>
     )
 }
