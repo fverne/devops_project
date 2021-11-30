25671dfc64e8dd8dbb4999a90e38caa32877bc86
fverne <frederikverne95@gmail.com>
2021-11-30 08:13:28 +0100

    maybe auth works

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -7,6 +7,10 @@ public class Config {
     public static final boolean DEBUG = true;
     public static final String CAMPUSNET_LOGIN_URL = "https://auth.dtu.dk/dtu/";
     public static final String CAMPUSNET_VALIDATE_URL = "https://auth.dtu.dk/dtu/validate";
+
+    // custom path to our site
+    public static final String GODINSIDE = "https://godinside.devops.diplomportal.dk/";
+
     //service paths
     public static final String BASE_API_PATH = "/rest";
     //V1 paths
