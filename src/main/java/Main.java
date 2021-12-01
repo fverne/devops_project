bc6ba2e4b6a877fd8156d8b619fbaaaddc2fe868
fverne <frederikverne95@gmail.com>
2021-12-01 11:23:23 +0100

    tried factory version of httpserver

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
+import Services.Metrics;
 import io.prometheus.client.exporter.HTTPServer;
-import io.prometheus.client.hotspot.DefaultExports;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
@@ -8,9 +8,10 @@ import java.io.IOException;
 import java.util.Optional;
 
 public class Main {
+    Metrics metrics = new Metrics(); //needed to start monitoring
+
     public static void main(String[] args) throws IOException {
-        DefaultExports.initialize();
-        HTTPServer prometheusServer = new HTTPServer.Builder().withPort(19998).build();
+        HTTPServer prometheusServer = new HTTPServer(19998);
 
         Tomcat tomcat = new Tomcat();
         tomcat.setBaseDir("temp");
